efb2f368df7b7614f88b9fb419bfb118ace94a67
Daniel Heriberto Palencia Arreola <buzoherbert@gmail.com>
2017-11-10 12:33:48 -0500

    Added all the variables to the model and considering them as categorical

--- analysis.r
+++ analysis.r
@@ -11,14 +11,6 @@ library(ggplot2);
 library(lubridate);
 
 
-# install.packages("GGally", dependencies = TRUE);
-
-library(NISTunits);
-# Graphing library
-# library(GGally)
->>>>>>> origin/master
-
-
 # loading file
 safety_data = read.csv("safety_data.csv");
 
@@ -50,34 +42,59 @@ haversine <- function(data, lat1, lon1, lat2, lon2){
 
 safety_data[["haversine"]] = haversine(safety_data, "cetram_lat", "cetram_long", "latitude", "longitude")
 
+# get hours
+
+times = strptime(safety_data$time, "%I:%M:%S %p");
+
+safety_data$hour = as.factor(hour(times));
+safety_data = subset(safety_data, select = -c(time) )
+
 # Making a basic plot of some potentially relevant variables
 
 plot_data <- data.frame(
-  point_security = safety_data[["pointsecurity"]],
-  haversine = safety_data[["haversine"]],
+  bus_or_ped = safety_data[["bus_or_ped"]],
+  base_study_zone = safety_data[["base_study_zone"]],
+  busdestination = safety_data[["busdestination"]],
+  inside_or_outside = safety_data[["inside_or_outside"]],
+  total_seats = safety_data[["totalseats"]],
+  total_passenger_count = safety_data[["totalpassengercount"]],
+  total_female_count = safety_data[["totalfemalecount"]],
+  empty_seats = safety_data[["emptyseats"]],
   gender = safety_data[["gender"]],
   age = safety_data[["age"]],
   companions = safety_data[["companions"]],  
   education = safety_data[["educational_attainment"]],
+  origin = safety_data[["origin"]],
+  destination = safety_data[["destinations"]],
   trip_purpose = safety_data[["trip_purpose"]],
-  #time = as.Date(safety_data[["time"]]),
-  base_study_zone = safety_data[["base_study_zone"]],
-  busdestination = safety_data[["busdestination"]],
-  inside_or_outside = safety_data[["inside_or_outside"]]
-  );
+  mode_security = safety_data[["modesecurity"]],
+  point_security = safety_data[["pointsecurity"]],
+  importance_safety = safety_data[["Importance_safety_digit"]],
+  most_safe = safety_data[["mostsafe"]],
+  least_safe = safety_data[["leastsafe"]],
+  urban_typology = safety_data[["urban.typology"]],
+  haversine = safety_data[["haversine"]],
+  hour = safety_data[["hour"]]
+);
 
-# get hours
+# Treating all the variables as categorical
+for(i in names(plot_data)){
+  plot_data[[i]] <- as.factor(plot_data[[i]])
+}
 
-times = strptime(plot_data$time, "%I:%M:%S %p");
+plot_data[["point_security"]] = as.numeric(as.character(plot_data[["point_security"]]))
 
-plot_data$hour = hour(times);
+
+
+
 
 # Getting a summary of the data
 summary(plot_data)
 
 # plotting the data
-
-ggpairs(plot_data, mapping = aes(color = point_security))
+to_plot = plot_data;
+to_plot$point_security = as.factor(to_plot$point_security)
+ggpairs(to_plot, mapping = aes(color = point_security))
 
 
 # ggpairs(plot_data,)
