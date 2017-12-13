08adb536299384866693abfbbbf00182c0db4f3c
Daniel Heriberto Palencia Arreola <buzoherbert@gmail.com>
2017-12-12 23:28:28 -0500

    Exported summaries

--- metrics/metrics.r
+++ metrics/metrics.r
@@ -1,5 +1,8 @@
 install.packages("plyr")
+install.packages("ggplot2")
+
 library(plyr)
+library(ggplot2)
 
 metrics = NULL
 
@@ -22,3 +25,38 @@ for (file in file_list){
     }
   }
 }
+
+
+
+metrics$category = revalue(metrics$category, c("Trip context"="Contextual", "Personal trip"="Trip", "Time context" = "Instant", "All variables" = "All"))
+
+
+
+variables = list("osr2", "accuracy", "sse", "f1_score")
+
+
+p <- ggplot(metrics, aes(category, osr2, label = model))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/cat_osr2.png")
+p <- ggplot(metrics, aes(category, accuracy, label = model))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/cat_acc.png")
+p <- ggplot(metrics, aes(category, sse, label = model))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/cat_sse.png")
+p <- ggplot(metrics, aes(category, f1_score, label = model))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/cat_f1.png")
+
+p <- ggplot(metrics, aes(model, osr2, label = category))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/mod_osr2.png")
+p <- ggplot(metrics, aes(model, accuracy, label =category))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/mod_acc.png")
+p <- ggplot(metrics, aes(model, sse, label = category))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/mod_sse.png")
+p <- ggplot(metrics, aes(model, f1_score, label = category))
+p + geom_boxplot()  + geom_point() + geom_text(hjust = 0, nudge_x = 0.05, check_overlap = FALSE, size = 3, angle = 30)+ theme(axis.text.x = element_text(size=10, angle=30))
+ggsave("./graphs/mod_f1.png")
